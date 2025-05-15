4ee7ea24796a9898354e868fc8fd436e490f4ed1
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2025-05-15 13:26:07 +0000

    Solved day 6! albeit with a bit of exxternal help

--- Day6/src/main.rs
+++ Day6/src/main.rs
@@ -1,91 +1,158 @@
 use std::fs::read_to_string;
+use std::collections::HashSet;
+
+#[derive(Clone, PartialEq, Hash, Eq, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+impl Direction {
+    fn turn_right(&mut self) {
+        *self = match self {
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
+        }
+    }
+}
+
+struct Grid {
+    grid: Vec<Vec<char>>,
+    height: usize,
+    width: usize,
+}
+
+impl Grid {
+    fn new() -> Self {
+        let grid = read_to_string("input.txt")
+            .expect("Failed to read file")
+            .lines()
+            .map(|line| line.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let height = grid.len();
+        let width = grid[0].len();
+        Grid { grid, height, width }
+    }
+
+    fn get_pos(&self) -> (usize, usize) {
+        for i in 0..self.height {
+            for j in 0..self.width {
+                if self.grid[i][j] == '^' {
+                    return (i, j);
+                }
+            }
+        }
+        panic!("guard found");
+    }
+
+    fn next_pos(&self, (guardx, guardy): (usize, usize), direction: &mut Direction) -> Option<(usize, usize)> {
+       let (nextx, nexty) = match direction {
+            Direction::Up => (guardx.checked_sub(1)?, guardy),
+            Direction::Down => (guardx + 1, guardy),
+            Direction::Left => (guardx, guardy.checked_sub(1)?),
+            Direction::Right => (guardx, guardy + 1),
+        };
+
+        let next_tile = self.grid.get(nextx).and_then(|row| row.get(nexty))?;
+
+        if next_tile == &'#' {
+            direction.turn_right();
+            return Some((guardx, guardy));
+        }
+
+        Some((nextx, nexty))
+    }
+}
 
 fn main() {
-    let mut grid = read_to_string("input.txt").expect("Failed to read file").lines().map(|line| line.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    
-    let (mut curx, mut cury) = (0,0);
-    for i in 0..grid.len() {
-        for j in 0..grid[i].len() {
-            if grid[i][j] == '^' {
-                curx = i;
-                cury = j;
+    let mut grid = Grid::new();
+    let (mut guardx, mut guardy) = grid.get_pos();
+    let mut direction = Direction::Up;
+
+    let mut path = HashSet::new();
+    path.insert((guardx, guardy));
+
+    while let Some((nextx, nexty)) = grid.next_pos((guardx, guardy), &mut direction) {
+        
+        path.insert((nextx, nexty));
+
+        guardx = nextx;
+        guardy = nexty;
+    }
+    let tot = path.len();
+    println!("Total path length: {}", tot);
+
+    // part 2
+    // let (mut guardx, mut guardy) = grid.get_pos();
+    // let mut direction = Direction::Up;
+
+    // let mut visited = HashSet::new();
+    // let mut count = 0;
+
+    // while let Some((nextx, nexty)) = grid.next_pos((guardx, guardy), &mut direction) {
+    //     visited.insert((nextx, nexty));
+
+    //     if !visited.contains(&(nextx, nexty)) {
+    //         grid.grid[nextx][nexty] = 'X';
+    //         if makes_loop(&grid, (nextx, nexty), direction) {
+    //             count += 1;
+    //         }
+    //         grid.grid[nextx][nexty] = '.';
+    //     }
+    //     (guardx, guardy) = (nextx, nexty);
+    // }
+
+    println!("Total loops: {}", part2());
+
+}
+
+pub fn part2() -> usize {
+    let mut grid = Grid::new();
+    let (mut guard_row, mut guard_col) = grid.get_pos();
+    let mut direction = Direction::Up;
+
+    let mut visited = HashSet::new();
+    let mut count = 0;
+
+    while let Some((next_row, next_col)) = grid.next_pos((guard_row, guard_col), &mut direction)
+    {
+        visited.insert((guard_row, guard_col));
+
+        if !visited.contains(&(next_row, next_col)) {
+            grid.grid[next_row][next_col] = '#';
+            if makes_loop(&grid, (guard_row, guard_col), direction) {
+                count += 1;
             }
+            grid.grid[next_row][next_col] = '.';
         }
+
+        (guard_row, guard_col) = (next_row, next_col);
     }
-    mover(&mut grid, curx, cury, 'U');
-    let tot = grid.iter().map(|row| row.into_iter().filter(|&c| *c == 'X').count()).sum::<usize>();
-    println!("Total: {}", tot);
+
+    count
 }
 
-fn mover(grid: &mut Vec<Vec<char>>, curx: usize, cury: usize, direction: char){
-    let (mut x, mut y, mut dir) = (curx, cury, direction);
-    let mut motion_flag = true;
-    while motion_flag {
-        let (nextx, nexty) = match dir {
-            'U' => (x - 1, y),
-            'D' => (x + 1, y),
-            'L' => (x, y - 1),
-            'R' => (x, y + 1),
-            _ => (x, y),
-        };
-        if nextx >= grid.len() || nexty >= grid[0].len() || grid[nextx][nexty] == '#' || nextx < 0 || nexty < 0 {
-            if match_turn(direction, &x, &y, &grid) == '#' || oor(direction, &x, &y, &grid.len(), &grid[0].len()) {
-                motion_flag = false
+fn makes_loop(grid: &Grid, (startx, starty): (usize, usize), start_direction: Direction) -> bool {
+    let mut visited_obstacles: Vec<(usize, usize, Direction)> = Vec::new();
+
+    let mut direction = start_direction;
+    let (mut guard_row, mut guard_col) = (startx, starty);
+
+    while let Some((next_row, next_col)) = grid.next_pos((guard_row, guard_col), &mut direction)
+    {
+        if (guard_row, guard_col) == (next_row, next_col) {
+            if visited_obstacles.contains(&(guard_row, guard_col, direction)) {
+                return true;
             }
-            grid[x][y] = 'X';
-            dir = turn(dir);
-            
-            
-        } else {
-            grid[x][y] = 'X';
-            grid[nextx][nexty] = '^';
-            x = nextx;
-            y = nexty;
+
+            visited_obstacles.push((guard_row, guard_col, direction));
         }
-    }
-}
-fn turn(direction: char) -> char {
-    match direction {
-        'U' => 'R',
-        'D' => 'L',
-        'L' => 'U',
-        'R' => 'D',
-        _ => direction,
-    }
-}
 
-fn match_turn(direction: char, x: &usize, y: &usize, grid : &Vec<Vec<char>>) -> char {
-    if oor(direction, x, y, &grid.len(), &grid[0].len()) {
-        return '#';
+        (guard_row, guard_col) = (next_row, next_col);
     }
-    let next = match direction {
-        'U' => 'R',
-        'D' => 'L',
-        'L' => 'U',
-        'R' => 'D',
-        _ => direction,
-    };
-    let (nextx, nexty) = match next {
-        'U' => (x - 1, *y),
-        'D' => (x + 1, *y),
-        'L' => (*x, y - 1),
-        'R' => (*x, y + 1),
-        _ => (*x, *y),
-    };
-    grid[nextx][nexty]
-}
 
-fn oor(direction: char, x: &usize, y: &usize, x_lim: &usize, y_lim: &usize) -> bool {
-    let (nextx, nexty) = match direction {
-        'U' => (x - 1, *y),
-        'D' => (x + 1, *y),
-        'L' => (*x, y - 1),
-        'R' => (*x, y + 1),
-        _ => (*x, *y),
-    };
-    if nextx >= *x_lim || nexty >= *y_lim || nextx < 0 || nexty < 0 {
-        return true;
-    }
     false
 }
